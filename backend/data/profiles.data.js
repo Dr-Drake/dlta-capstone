bd05f75028e6385651f9dc89b1f48dc67c3524c4
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-23 17:31:37 +0200

    Add tests

---
+++ backend/data/profiles.data.js
@@ -0,0 +1,99 @@
+export   const profiles = [
+    {
+      _id: "6419e4a01504645521cfe757",
+      name: "Mugisha Samuel",
+      picture: "https://i.postimg.cc/vmx4Vqdh/mugisha-samuel.png",
+      role: "Software Engineer",
+      bio: "A multi-skilled Software engineer with hands-on experience in developing, and confounding tech startups (B2B & Customer-centric) by bringing together the vision, strategy, and execution in a totally unstructured and resource-constrained environment.",
+      location: "Rwanda",
+      projects: [
+        {
+          projectName: "React Todo-list",
+          youtube: "https://youtu.be/yYdb-reJ_DM",
+          github: "https://github.com/mugishasam123/Todolist",
+          description:
+            "Todo list is an app that allows you to track all of your daily activities by setting up activities,editing and removing them",
+          technologies: ["Javascript", "Tailwind", "Vite"],
+          _id: "6419e4a01504645521cfe758",
+        },
+        {
+          projectName: "React Quiz App",
+          youtube: "https://youtu.be/fqsJHtGX9D0",
+          github: "https://github.com/mugishasam123/simple-quiz-app",
+          description:
+            "Quiz app is an app that allows you to play a game of answering multiple choice questions and provides your scores at the end",
+          technologies: ["Javascript", "Tailwind", "Vite"],
+          _id: "6419e4a01504645521cfe759",
+        }
+      ],
+      __v: 0,
+      createdAt: "2023-03-21T17:08:48.639Z",
+      updatedAt: "2023-03-21T17:08:48.639Z",
+    },
+    {
+      _id: "6419e4a01504645521cfe766",
+      name: "NSABIMANA Emmanuel",
+      picture: "https://i.postimg.cc/9MjQqcbX/Emmy.jpg",
+      role: "Software Engineer",
+      bio: "Persistent and ambitious software engineer with a desire to grow fast in this domain. Capable of ramping up quickly and efficiently, and of both working in a team and independently. Lover of remote work. Skilled in various languages, frameworks, and technologies. Built 30+ projects while completing Microverse, an online software development school that accepts less than 1% of its applicants.",
+      location: "Rwanda",
+      projects: [
+        {
+          projectName: "QR Code Generator",
+          youtube: "https://youtu.be/Ckk4L_3ClMI",
+          github:
+            "https://github.com/Emmyn5600/delta-project/tree/main/qrcodegenerator",
+          description:
+            "QR code generator is terminal app that helps to create a QR code from the data provided either as image or directly in terminal",
+          technologies: ["Javascript", "Node.js"],
+          _id: "6419e4a01504645521cfe767",
+        },
+        {
+          projectName: "Rock-paper Scissors App",
+          youtube: "https://youtu.be/7k7ai1TO4j0",
+          github:
+            "https://github.com/Emmyn5600/delta-project/tree/main/Rock-Paper-Scissors",
+          description:
+            "Game of the rock-scissors-paper.It works in the following way: Stone wins over scissors because stone is hard and cannot be cut with scissors. Scissors win over paper because scissors can cut paper. Paper wins over stone because paper can wrap stone.",
+          technologies: ["Javascript", "HTML", "CSS"],
+          _id: "6419e4a01504645521cfe768",
+        },
+       
+      ],
+      __v: 0,
+      createdAt: "2023-03-21T17:08:48.642Z",
+      updatedAt: "2023-03-21T17:08:48.642Z",
+    },
+    {
+      _id: "6419e4a01504645521cfe775",
+      name: "Chance Desire IRADUKUNDA",
+      picture: "https://i.postimg.cc/CM9SZhx3/Desire.jpg",
+      role: "Software Engineer",
+      bio: "Software Engineer & Full-Stack Developer | Blockchain Enthusiastic, IT",
+      location: "Rwanda",
+      projects: [
+        {
+          projectName: "React Todo-list",
+          youtube: "https://www.youtube.com/watch?v=Tge3AhTl-xQ",
+          github: "https://github.com/chanceDira/todo-challenge",
+          description:
+            "Todo list is an app that allows you to track all of your daily activities by setting up activities,editing and removing them",
+          technologies: ["Javascript", "CSS", "HTML"],
+          _id: "6419e4a01504645521cfe776",
+        },
+        {
+          projectName: "Phaser Simple Game",
+          youtube: "https://www.youtube.com/watch?v=i3YrglLWu40",
+          github: "https://github.com/chanceDira/Juggle-PhaserJS",
+          description:
+            "This a short and simple game built using phaser3 JS.",
+          technologies: ["Javascript", "Phaser"],
+          _id: "6419e4a01504645521cfe777",
+        },
+        
+      ],
+      __v: 0,
+      createdAt: "2023-03-21T17:08:48.643Z",
+      updatedAt: "2023-03-21T17:08:48.643Z",
+    },
+  ];
